ca5b1166266a1b7e66cb59d705c6744366f86915
RudraCodeForge <jitandradaksh533@gmail.com>
2026-09-06 00:02:09 +0530

    Add Adimn and admin otp model

---
+++ Backend/Models/AdminOTP.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const AdminOTPSchema = new mongoose.Schema(
+  {
+    adminId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Admin",
+      required: true,
+      index: true,
+    },
+
+    otpHash: {
+      type: String,
+      required: true,
+    },
+
+    expiresAt: {
+      type: Date,
+      required: true,
+      index: true,
+    },
+
+    attempts: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// Automatically delete OTP after expiry
+AdminOTPSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
+
+module.exports = mongoose.model("AdminOTP", AdminOTPSchema);
